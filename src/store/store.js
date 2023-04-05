4eb9315f592bbaf02d7274e5424056394cb31b7c
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-04-05 15:49:30 +0000

    Create a new slice for uses

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
+import usersReducer from './users/usersSlice';
 
 const store = configureStore({
   reducer: {
---
+++ src/store/users/usersSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  users: [],
+  isLoading: true,
+  error: undefined
+}
+
+const usersSlice = createSlice({
+  name: 'users',
+  initialState,
+  extraReducers: {
+
+  }
+});
+
+export default usersSlice.reducer;
